a9078278e6228fb9117aa3ec872da80764cb192a
misratasci <misratasci@gmail.com>
2024-01-04 15:58:54 +0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,22 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:30:31 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/28 18:08:34 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/01/04 13:54:18 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
-# include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
 
 char	*get_next_line(int fd)
 {
-	size_t			buff_size;
 	void			*buffer;
 	static size_t	l;
 
-	buff_size = BUFFER_SIZE;
-	buffer = (char *)malloc(buff_size * sizeof(char));
-	l = read(fd, buffer, buff_size);
+	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	l = read(fd, buffer, BUFFER_SIZE);
 	return (buffer);
 }
 
@@ -30,7 +30,7 @@ char	*get_next_line(int fd)
 int main()
 {
 	int fd = open("a.txt", O_RDONLY);
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
+	printf("1. %s\n", get_next_line(fd));
+	printf("2. %s\n", get_next_line(fd));
+	printf("3. %s\n", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,9 +6,13 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:31:10 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/28 18:14:48 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/01/04 13:43:33 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+#endif
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
