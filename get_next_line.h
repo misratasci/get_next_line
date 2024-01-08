916e8d77f45f9d1745f71cf35bb16a67f3a7b928
misratasci <misratasci@gmail.com>
2024-01-08 20:46:48 +0300

    a

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:31:10 by mitasci           #+#    #+#             */
-/*   Updated: 2024/01/04 13:43:33 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/01/08 20:45:14 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE
+# define BUFFER_SIZE 35
 #endif
 
 #ifndef GET_NEXT_LINE_H
